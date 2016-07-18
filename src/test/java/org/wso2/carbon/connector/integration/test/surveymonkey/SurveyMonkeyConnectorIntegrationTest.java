eae7691212f41d1ca154b98a16df9240c657a1aa
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2016-07-18 17:55:47 +0530

    To release the tag

--- src/test/java/org/wso2/carbon/connector/integration/test/surveymonkey/SurveyMonkeyConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/surveymonkey/SurveyMonkeyConnectorIntegrationTest.java
@@ -41,7 +41,7 @@ public class SurveyMonkeyConnectorIntegrationTest extends ConnectorIntegrationTe
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("surveymonkey-connector-1.0.1-SNAPSHOT");
+        init("surveymonkey-connector-1.0.1");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
